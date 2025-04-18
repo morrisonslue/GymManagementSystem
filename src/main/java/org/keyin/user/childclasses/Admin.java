ead97351d65a8b36dfda1d8f181aba58003164fe
morrisonslue <chris@morrisonslue.com>
2025-04-17 21:37:11 -0230

    added README and several small fixes

--- src/main/java/org/keyin/user/childclasses/Admin.java
+++ src/main/java/org/keyin/user/childclasses/Admin.java
@@ -1,6 +1,7 @@
-package org.keyin.user.childclasses;
-
-import org.keyin.user.User;
-
-public class Admin extends User {
-}
+// package org.keyin.user.childclasses;
+//
+// // not used in project – decided to manage user roles directly via the User
+// class
+// public class Admin {
+// // originally planned for role specific logic but user role strings worked
+// }
